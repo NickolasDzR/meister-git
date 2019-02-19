3215ad7196fb67d2df3c0bfe2d8fa708f140927c
NickolasDzR <42074447+NickolasDzR@users.noreply.github.com>
2019-02-19 06:06:27 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 var link = document.querySelector(".call-button");
 var popup = document.querySelector(".form-unshow");
-var close = document.querySelector(".popup-close")
+var close = document.querySelector(".popup-close");
+var input_name = popup.querySelector("[name=name]");
+var input_number = popup.querySelector("[name=number]");
+
+var form = popup.querySelector("form");
 
 link.addEventListener( "click", function(evt){
   evt.preventDefault();
 popup.classList.add("form-show");
+input_name.focus();
 popup.classList.remove("form-unshow");
   console.log("включаем popup добавляя form-show и удаляя form-unshow");
 });
@@ -14,4 +19,11 @@ close.addEventListener( "click", function(evt){
   popup.classList.remove("form-show");
   popup.classList.add("form-unshow");
     console.log("выключаем popup добавляя form-unshow и удаляя form-show");
+});
+
+form.addEventListener("submit", function(evt) {
+  if (!input_name.value || !input_number.value) {
+  evt.preventDefault();
+  console.log("Нужно ввести пароль");
+  }
 });
