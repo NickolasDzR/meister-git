8a8a7b6a491fc6a239c92a90667f5d492ba4cae5
NickolasDzR <42074447+NickolasDzR@users.noreply.github.com>
2019-02-20 08:28:11 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -3,37 +3,55 @@ var popup = document.querySelector(".form-unshow");
 var close = document.querySelector(".popup-close");
 var input_name = popup.querySelector("[name=name]");
 var input_number = popup.querySelector("[name=number]");
+var form = popup.querySelector(".popup-form");
 
-var form = popup.querySelector("form");
+var form_request = document.querySelector(".col-7-fm");
+var order_form = form_request.querySelector(".order-form");
+var input_company = form_request.querySelector("[name=company]");
+var input_your_point = form_request.querySelector("[name=your-point]");
+var input_cargo_weight = form_request.querySelector("[name=cargo-weight]");
+var input_phone_email = form_request.querySelector("[name=phone-email]");
+var input_destination_point = form_request.querySelector("[name=destination-point]");
+var input_comment = form_request.querySelector("[name=comment]");
 
-link.addEventListener( "click", function(evt){
+order_form.addEventListener("submit", function (evt) {
+  if (!input_company.value || !input_your_point.value || !input_cargo_weight.value || !input_phone_email.value || !input_destination_point.value || !input_comment) {
+    evt.preventDefault();
+    form_request.classList.remove("modal-error");
+    form_request.offsetWidth = popup.offsetWidth;
+    form_request.classList.add("modal-error");
+    console.log("Нужно ввести пароль");
+  }
+});
+
+link.addEventListener("click", function (evt) {
   evt.preventDefault();
-popup.classList.add("form-show");
-input_name.focus();
-popup.classList.remove("form-unshow");
+  popup.classList.add("form-show");
+  input_name.focus();
+  popup.classList.remove("form-unshow");
   console.log("включаем popup добавляя form-show и удаляя form-unshow");
 });
 
-close.addEventListener( "click", function(evt){
+close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("form-show");
   popup.classList.remove("modal-error");
   popup.classList.add("form-unshow");
-    console.log("выключаем popup добавляя form-unshow и удаляя form-show");
+  console.log("выключаем popup добавляя form-unshow и удаляя form-show");
 });
 
-form.addEventListener("submit", function(evt) {
+form.addEventListener("submit", function (evt) {
   if (!input_name.value || !input_number.value) {
-  evt.preventDefault();
-  popup.classList.remove("modal-error");
-  popup.offsetWidth = popup.offsetWidth;
-  popup.classList.add("modal-error");
-  console.log("Нужно ввести пароль");
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+    console.log("Нужно ввести пароль");
   }
 });
 
-window.addEventListener("keydown", function(evt) {
-  if (evt.keyCode === 27 ){
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
     if (popup.classList.contains("form-show")) {
       evt.preventDefault();
       popup.classList.remove("form-show");
