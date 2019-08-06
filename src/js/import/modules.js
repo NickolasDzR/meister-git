d92c02b211714b2e0bbc1fbf50a34fdb28210efa
Nickolas <nikolyae2019@gmail.com>
2019-08-06 14:09:49 +0300

    b-advantages

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -2,3 +2,4 @@ import "%modules%/main-container/main-container.js";
 import "%modules%/header/header.js";
 import "%modules%/main/main.js";
 import "%modules%/main/slider/slider.js";
+import "%modules%/main/b-about/b-about.js";
