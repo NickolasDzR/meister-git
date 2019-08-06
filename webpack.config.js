127b5188d5f4652239a145d25b50b560f012e06a
Nickolas <nikolyae2019@gmail.com>
2019-08-06 12:07:23 +0300

    replaced bootstrap, made info block, and a few another updates

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,39 @@ module.exports = {
         publicPath: "/"
     },
 
+    module: {
+        rules: [
+          {
+            test: /\.(scss)$/,
+            use: [
+              {
+                // Adds CSS to the DOM by injecting a `<style>` tag
+                loader: 'style-loader'
+              },
+              {
+                // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                loader: 'css-loader'
+              },
+              {
+                // Loader for webpack to process CSS with PostCSS
+                loader: 'postcss-loader',
+                options: {
+                  plugins: function () {
+                    return [
+                      require('autoprefixer')
+                    ];
+                  }
+                }
+              },
+              {
+                // Loads a SASS/SCSS file and compiles it to CSS
+                loader: 'sass-loader'
+              }
+            ]
+          }
+        ]
+      },
+
     plugins: [
         new webpack.ProvidePlugin({
           $: 'jquery',						
@@ -49,6 +82,7 @@ module.exports = {
             }
         ]
     },
+    
 
     resolve: {
         alias: {
@@ -57,3 +91,5 @@ module.exports = {
         }
     }
 };
+
+
