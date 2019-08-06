19f180f2b93410c04aeaec52f72b9bf8ae1847c2
Nickolas <nikolyae2019@gmail.com>
2019-08-06 20:41:05 +0300

    make form

---
+++ src/blocks/modules/main/b-about/b-about.js
@@ -0,0 +1,19 @@
+import $ from "jquery";
+
+$(function(){
+    $("form.callback").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "../mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$(th).find(".success").addClass("active").css("display", "flex").hide().fadeIn();
+			setTimeout(function() {
+				$(th).find("success").removeClass("active").fadeOut();
+				th.trigger("reset");
+			}, 2000);
+		});
+		return false;
+	});
+});
