c23d728b0c17a7ccf1c70b66eba5d34fa90014d6
Nickolas <nikolyae2019@gmail.com>
2019-08-08 23:03:20 +0300

    making pop up in a process

--- src/blocks/components/popup/popup.js
+++ src/blocks/components/popup/popup.js
@@ -1,23 +1,49 @@
 import $ from "jquery";
 import Swal from "sweetalert2";
 
+$('.popup-btn').click(function () {
+  event.preventDefault();
+  $('.remodal').addClass('remodal-is-active');
+  $('.overlay-wrapper').addClass('overlay');
+});
 
+$('#popup-btn').click(function () {
+  event.preventDefault();
+  $('.remodal').removeClass('remodal-is-active');
+  $('.overlay-wrapper').removeClass('overlay');
+});
 
-$('.confirm').click(function () {
-  if ($(this).closest('.remodal').find('.callback input[name="Имя"]' && '.callback input[name="Номер телефона"]').val() != '') {
+$('.remodal-close').click(function () {
+  event.preventDefault();
+  $('.remodal').removeClass('remodal-is-active');
+  $('.overlay-wrapper').removeClass('overlay');
+});
+
+$('#popup-btn').click(function () {
+  if ($(this).closest('.remodal').find('.callback input[name="Имя"]').val() === '') {
+    Swal.fire({
+      type: 'error',
+      title: 'Оой...',
+      text: 'Заполните все поля!',
+    });
+    $('.swal2-actions button').on('click', function () {
+      $('.header_contacts a')[0].click();
+    });
+  } else if ($(this).closest('.remodal').find('.callback input[name="Телефон"]').val() === '') {
+    Swal.fire({
+      type: 'error',
+      title: 'Оой...',
+      text: 'Заполните все поля!',
+    });
+    $('.swal2-actions button').on('click', function () {
+      $('.header_contacts a')[0].click();
+    });
+  } else {
     Swal.fire(
       'Заявка отправленна!',
       'Мы вам скоро перезвоним',
       'success'
     )
-  } else {
-    Swal.fire({
-      type: 'error',
-      title: 'Оой...',
-      text: 'Заполните все поля!',
-    })
-    $('.swal2-actions button').on('click', function() {
-      $('.btn-wrapper a')[0].click();
-  });
+    $('.overlay-wrapper').removeClass('overlay');
   }
 });
--- src/js/import/components.js
+++ src/js/import/components.js
@@ -1,3 +1,2 @@
 import '../../../node_modules/owl.carousel/dist/owl.carousel.min'
-import '../../../node_modules/remodal/dist/remodal.min'
 import '../../../node_modules/sweetalert2/dist/sweetalert2.all'
