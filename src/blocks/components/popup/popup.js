235ce272cf6fca75aa61be5b30876e63c1aee36b
Nickolas <nikolyae2019@gmail.com>
2019-08-08 09:26:13 +0300

    commit input

--- src/blocks/components/popup/popup.js
+++ src/blocks/components/popup/popup.js
@@ -1,44 +1,25 @@
 import $ from "jquery";
 import Swal from "sweetalert2";
 
-$(function () {
 
-    // In this case the initialization function returns the already created instance
-    $('[data-remodal-id=modal2]').remodal({
-        modifier: 'with-red-theme'
+
+$('.confirm').click(function () {
+  if ($(this).closest('.remodal').find('.callback input[name="Имя"]' || '.callback input[name="Номер телефона"]').val() != '') {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
     });
+    Toast.fire({
+      type: 'success',
+      title: 'Заявка принята'
+    });
+  } else {
+    Swal.fire({
+      type: 'error',
+      title: 'Oops...',
+      text: 'Заполните все поля!',
+    })
+  }
 });
-
-$('.confirm').click(function(){ 
-    if($(this).closest('.remodal').find('.callback input[name="Имя"]').val() != ''){ 
-        const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            scrollbarPadding: false,
-            timer: 3000
-          });
-          
-          Swal.fire(
-            'Спасибо за заявку!',
-            'Мы Вам скоро перезвоним',
-            'success'
-          )
-    } else { 
-      console.log('пусто')
-    }; 
-  }); 
-
-// $(function() {
-//     $('.confirm').click(function(){
-//         if( $(input).val().length == 0 ) {
-//             Swal.fire({
-//                 position: 'top-end',
-//                 type: 'success',
-//                 title: 'Your work has been saved',
-//                 showConfirmButton: false,
-//                 timer: 1500
-//               });
-//         }
-//     });
-// });
