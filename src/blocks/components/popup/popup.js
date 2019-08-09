f27bd5f2a5bc92dc8d1e73a46c7fc3701b064080
Nickolas <nikolyae2019@gmail.com>
2019-08-09 13:06:32 +0300

    jquery popup

--- src/blocks/components/popup/popup.js
+++ src/blocks/components/popup/popup.js
@@ -2,48 +2,76 @@ import $ from "jquery";
 import Swal from "sweetalert2";
 
 $('.popup-btn').click(function () {
-  event.preventDefault();
-  $('.remodal').addClass('remodal-is-active');
-  $('.overlay-wrapper').addClass('overlay');
-});
-
-$('#popup-btn').click(function () {
-  event.preventDefault();
-  $('.remodal').removeClass('remodal-is-active');
-  $('.overlay-wrapper').removeClass('overlay');
-});
-
-$('.remodal-close').click(function () {
-  event.preventDefault();
-  $('.remodal').removeClass('remodal-is-active');
-  $('.overlay-wrapper').removeClass('overlay');
-});
-
-$('#popup-btn').click(function () {
-  if ($(this).closest('.remodal').find('.callback input[name="Имя"]').val() === '') {
-    Swal.fire({
-      type: 'error',
-      title: 'Оой...',
-      text: 'Заполните все поля!',
-    });
-    $('.swal2-actions button').on('click', function () {
-      $('.header_contacts a')[0].click();
-    });
-  } else if ($(this).closest('.remodal').find('.callback input[name="Телефон"]').val() === '') {
-    Swal.fire({
-      type: 'error',
-      title: 'Оой...',
-      text: 'Заполните все поля!',
-    });
-    $('.swal2-actions button').on('click', function () {
-      $('.header_contacts a')[0].click();
-    });
+  if (!$('.remodal').hasClass('remodal-is-active')) {
+    $('.remodal').addClass('remodal-is-active');
+    $('body').css('overflow', 'hidden');
+    $('body').css('padding-right', '17px');
+    $('#overlay').css('display', 'block');
   } else {
-    Swal.fire(
-      'Заявка отправленна!',
-      'Мы вам скоро перезвоним',
-      'success'
-    )
-    $('.overlay-wrapper').removeClass('overlay');
+      $('.remodal').removeClass('remodal-is-active');
+      $('body').css('overflow', '');
+      $('body').css('padding-right', '');
   }
 });
+
+// $('.popup-btn').click(function () {
+//   event.preventDefault();
+//   $('.remodal').addClass('remodal-is-active');
+//   $('body').css('overflow', 'hidden');
+//   $('body').css('padding-right', '17px');
+// });
+
+// $('#popup-btn').click(function () {
+//   event.preventDefault();
+//   $('.remodal').removeClass('remodal-is-active');
+//   $('body').css('overflow', '');
+//   $('body').css('padding-right', '');
+// });
+
+// $('.popup-btn').click(function () {
+//   $('#overlay').css('display', 'block');
+// });
+
+// $('#overlay').click(function () {
+//   $('#overlay').css('display', '');
+//   $('body').css('overflow', '');
+//   $('body').css('padding-right', '');
+//   $('.remodal').removeClass('remodal-is-active');
+// });
+
+// $('.remodal-close').click(function () {
+//   event.preventDefault();
+//   $('.remodal').removeClass('remodal-is-active');
+//   $('body').css('overflow', '');
+//   $('body').css('padding-right', '');
+//   $('#overlay').css('display', '');
+// });
+
+// $('#popup-btn').click('submit', function () {
+//   if ($(this).closest('.remodal').find('.callback input[name="Имя"]').val() === '') {
+//     Swal.fire({
+//       type: 'error',
+//       title: 'Оой...',
+//       text: 'Заполните все поля!',
+//     });
+//     $('.swal2-actions button').on('click', function () {
+//       $('.header_contacts a')[0].click();
+//     });
+//   } else if ($(this).closest('.remodal').find('.callback input[name="Телефон"]').val() === '') {
+//     Swal.fire({
+//       type: 'error',
+//       title: 'Оой...',
+//       text: 'Заполните все поля!',
+//     });
+//     $('.swal2-actions button').on('click', function () {
+//       $('.header_contacts a')[0].click();
+//     });
+//   } else {
+//     Swal.fire(
+//       'Заявка отправленна!',
+//       'Мы вам скоро перезвоним',
+//       'success'
+//     )
+//     $('.overlay-wrapper').removeClass('overlay');
+//   }
+// });
