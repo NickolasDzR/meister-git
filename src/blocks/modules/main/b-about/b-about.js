ee5fdc89cf62808b2d63e8e49b4afe6ada5d6769
Nickolas <nikolyae2019@gmail.com>
2019-08-16 11:49:16 +0300

    Last commit. update setTimeout form

--- src/blocks/modules/main/b-about/b-about.js
+++ src/blocks/modules/main/b-about/b-about.js
@@ -1,16 +1,16 @@
 import $ from "jquery";
 
-$(function(){
-    $("form.callback").submit(function() { //Change
+$(function () {
+	$("form.callback").submit(function () { //Change
 		var th = $(this);
 		$.ajax({
 			type: "POST",
 			url: "../mail.php", //Change
 			data: th.serialize()
-		}).done(function() {
-			$(th).find(".success").addClass("active").css("display", "flex").hide().fadeIn();
-			setTimeout(function() {
-				$(th).find("success").removeClass("active").fadeOut();
+		}).done(function () {
+			$(th).find(".success").addClass("blur-active").css("display", "flex").hide().fadeIn();
+			setTimeout(function () {
+				$(th).find(".success").removeClass("blur-active").fadeOut();
 				th.trigger("reset");
 			}, 2000);
 		});
